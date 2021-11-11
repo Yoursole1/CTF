a18d5471a4bd358315184e84d163e952af655521
BuildTools <unconfigured@null.spigotmc.org>
2021-11-11 09:14:32 -0800

    Removed Vector functions

--- src/main/java/me/yoursole/ctf/DataFiles/MathUtils.java
+++ src/main/java/me/yoursole/ctf/DataFiles/MathUtils.java
@@ -22,44 +22,5 @@ public class MathUtils {
     }
     public static double naturalLog(double arg){return Math.log(arg)/Math.log(2.71828);}
 
-    //Functions belongs to SurrendrX
-    public static Vector rotateVector(Vector vector, double angle) {
-        double sin = Math.sin(angle);
-        double cos = Math.cos(angle);
-        double x = vector.getX() * cos + vector.getZ() * sin;
-        double z = vector.getX() * -sin + vector.getZ() * cos;
-        return vector.setX(-x).setZ(-z);
-    }
-
-    public Vector getPosition(double distance, Vector origin, Vector direction) {
-        return origin.clone().add(direction.clone().multiply(distance));
-    }
-
-    public List<Vector> traverse(double distance, double accuracy, Vector origin, Vector direction) {
-        List<Vector> positions = new ArrayList();
-
-        for(double d = 0.0D; d <= distance; d += accuracy) {
-            positions.add(this.getPosition(d,origin,direction));
-        }
-
-        return positions;
-    }
-
-    public Block getIntersectedBlock(World world, double distance, double accuracy, Set<Material> transparent, Vector origin, Vector direction) {
-        Iterator var7 = this.traverse(distance, accuracy,origin,direction).iterator();
-
-        Block block;
-        do {
-            if (!var7.hasNext()) {
-                return null;
-            }
-
-            Vector vector = (Vector)var7.next();
-            block = world.getBlockAt(new Location(world, vector.getX(), vector.getY(), vector.getZ()));
-        } while(transparent.contains(block.getType()));
-
-        return block;
-    }
-    //end SurrendrX's functions
 
 }
