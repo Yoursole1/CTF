319bbcfc46ea4c8f79c4e4745fa348c6138d9960
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2021-11-21 15:12:09 -0500

    fix telekinesis?

--- src/main/kotlin/me/yoursole/ctf/events/DropItemEvent.kt
+++ src/main/kotlin/me/yoursole/ctf/events/DropItemEvent.kt
@@ -1,6 +1,6 @@
 package me.yoursole.ctf.events
 
-import me.yoursole.ctf.datafiles.items.Flag
+import me.yoursole.ctf.datafiles.Utils.getCTFId
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerDropItemEvent
@@ -8,7 +8,7 @@ import org.bukkit.event.player.PlayerDropItemEvent
 object DropItemEvent : Listener {
     @EventHandler
     fun onItemDrop(e: PlayerDropItemEvent) {
-        if (e.itemDrop.itemStack.isSimilar(Flag.flag)) {
+        if (e.itemDrop.itemStack.getCTFId() == "flag") {
             e.isCancelled = true
             e.player.sendMessage("§cYou can not get rid of this")
         }
